92eaeb96e59bfb3027060d73abbe68f68f1c6d15
Kaelin <octavian.cioanca1@gmail.com>
2026-07-02 03:13:11 +0300

    added proper phase, untested

--- SpiderBot.cpp
+++ SpiderBot.cpp
@@ -16,8 +16,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "hardware/pwm.h"
 //#include "hardware/timer.h"
 #include "pico/cyw43_arch.h"
-
 #include <math.h>
+
 
 //https://github.com/Harbys/pico-ssd1306
 #define SSD1306_ASCII_FULL
@@ -25,15 +25,15 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "pico-ssd1306/ssd1306.h"
 #include "pico-ssd1306/textRenderer/TextRenderer.h"
 
+
 #include "rawServo.h"
 
-float clampMax(float a, float b){
+float maxf(float a, float b){
     return a > b ? a : b;
 }
-float clampMin(float a, float b){
+float minf(float a, float b){
     return a < b ? a : b;
 }
-
 
 
 typedef struct vec3{
@@ -60,164 +60,22 @@ enum struct emotion{
 #define I2C_SDA 16
 #define I2C_SCL 17
 
-
-#define SERVO_CNT 8
-
-// both axes and regardless of side will make sure that they sit centered when servo is at half
-#define SERVO_CENTER 90
-
-// GPIO 14 is completely fried, not even a basic blink program makes it through
-uint servoPins[SERVO_CNT] = {6, 7, 8, 9, 10, 11, 12, 13};
-//uint servoSlices[SERVO_CNT];
+pico_ssd1306::SSD1306* display;
 
 
-typedef struct leg{
-    uint phiServo; // forward - back
-    uint thetaServo; // up - down
+void initDisplay(){
+    i2c_init(I2C_PORT, 1000000); //Use i2c port with baud rate of 1Mhz
 
-    void setPhi(float angle){
-        setAngleRad(phiServo, angle);
-        pose.x = angle;
-    };
-    void setTheta(float angle){
-        setAngleRad(thetaServo, angle);
-        pose.y = angle;
-    };
-
-    float phaseOffset = 0;
-
-    // stores its own height for reference and the servo angle that represents 0
-    // baseAngle is radians and only applies to height, not stride
-    float height = 0, baseAngle = M_PI/2;
-
-    // x/y, phi/theta
-    vec2 pose = {0, 0};
-    vec2 start = {0, 0}, target = {0, 0};
-}leg;
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
 
-
-#define BOT_WIDTH 80    // mm
-#define BOT_LENGTH 130
-
-// This is 
-#define ARM_LENGTH 60.0f
-// this is the actuator arm of the leg, as the 4 bar linkage acts as a static, 
-// constant offset for the robot's height
-
-// X forward, Y right, Z up
-
-const vec3 cornerFL = { BOT_LENGTH / 2, -BOT_WIDTH / 2, 0};
-const vec3 cornerFR = { BOT_LENGTH / 2,  BOT_WIDTH / 2, 0};
-const vec3 cornerBL = {-BOT_LENGTH / 2, -BOT_WIDTH / 2, 0};
-const vec3 cornerBR = {-BOT_LENGTH / 2,  BOT_WIDTH / 2, 0};
-
-leg legFL = {6, 7};
-leg legBL = {8, 9};
-leg legFR = {10, 11};
-leg legBR = {12, 13};
-
-leg* legs[4] = {
-    &legFL,
-    &legBL,
-    &legFR,
-    &legBR
-};
-
-vec2 legOrientations[] ={
-    vec2{-1, 1},
-    vec2{-1, 1},
-    vec2{1, -1},
-    vec2{1, -1}
-};
-
-
-double phaseFloat = 0;
-float trotOffset[] = {0.5, 0, 0, 0.5};
-float walkOffset[] = {0, 0.25, 0.5, 0.75};
-
-
-
-float getHeightAngle(float targetHeight){
-    /*
-    float tempAngle = 0;
-    float temp = 0; //middle
-    float range = M_PI_2;
-
-    // like a binary serch
-    while(fabs(targetHeight - temp) > 5){ // if current is within 5mm of target
-        
-        if(targetHeight > temp)                 // check if we're over or under
-            tempAngle += range * 0.5f;
-        else
-            tempAngle -= range * 0.5f;
-
-        temp = sinf(tempAngle) * ARM_LENGTH;  // generate condition //expensive operation
-            range /= 2;                         // shrink search area
-    }
-
-    return tempAngle;
-    */
-    return asin(targetHeight / ARM_LENGTH);
+    *display = pico_ssd1306::SSD1306(I2C_PORT, 0x3C, pico_ssd1306::Size::W128xH64);
+    display->setOrientation(true);
 }
-
-
-
-
-
-
-float chassisPitch = 0, chassisRoll = 0;
-
-// extracts the corner height out of Pitch x Roll x pos
-// assumes Z always starts at 0 AND corners are const
-float getCornerHeight(vec3 corner, float pitch, float roll){
-    float result =  -corner.x * sin(pitch) + 
-                    (corner.y * sin(roll) * cos(pitch));
-
-    return clampMax(clampMin(result, ARM_LENGTH - 15), -ARM_LENGTH + 5);
-}
-
-
-// mm, deg, deg
-void setPose(float height, float pitch, float roll){
-    legFL.height = legOrientations[0].x * (height + getCornerHeight(cornerFL, pitch, roll));
-    legBL.height = legOrientations[1].x * (height + getCornerHeight(cornerBL, pitch, roll));
-    legFR.height = legOrientations[2].x * (height + getCornerHeight(cornerFR, pitch, roll));
-    legBR.height = legOrientations[3].x * (height + getCornerHeight(cornerBR, pitch, roll));
-
-    // TODO: once i begin testing servos, make the proper baesAgles negative
-    legFL.baseAngle = M_PI_2 + getHeightAngle(legFL.height);
-    legFR.baseAngle = M_PI_2 + getHeightAngle(legFR.height);
-    legBL.baseAngle = M_PI_2 + getHeightAngle(legBL.height);
-    legBR.baseAngle = M_PI_2 + getHeightAngle(legBR.height);
-}
-
-
-// TODO: there are 6 transitions which need to be accounted for
-// from home to swing
-// from home to return
-// from swing to return
-// from swing to home
-// from return to swing
-// form return to home
-// find a way to account for this, hopefully without edge cases
-void setMovementType(moveType type){
-    
-}
-
-
-// defined as how much of a step is finished per second
-double timestep = 0;
-// -100 -> +100
-void setSpeed(float speed){
-    // timestep is a linear range between -2 and 2, where 2 means switching states twice per second
-    // this is probably way too fast, to be tuned later, TODO
-    timestep = speed / 50;
-}
-// on 0 throttle for half a second, return to standstill
-// real throttle should lag behind set throttle to prevent sudden shifts
-
-void setHeading(float yawRate);
 
 void setFace(emotion face, pico_ssd1306::SSD1306 *display){
     display->clear();
@@ -258,12 +116,217 @@ void setFace(emotion face, pico_ssd1306::SSD1306 *display){
 
 
 
+
+// TODO: give this the actual measurements of the robot
+#define BOT_WIDTH 80    // mm
+#define BOT_LENGTH 130
+#define ARM_LENGTH 60.0f //mm
+#define SERVO_CNT 8
+
+float chassisPitch = 0, chassisRoll = 0;
+
+
+#define SERVO_CENTER M_PI_2 // default home position
+#define SWING_HEIGHT 40 // raise leg by 40mm
+#define SWING_ARC_RAD 1  // swing leg 1 radian forward and back
+
+// on board used for testing, GPIO 14 is completely fried, not even a basic blink program makes it through
+uint servoPins[SERVO_CNT] = {6, 7, 8, 9, 10, 11, 12, 13};
+
+
+typedef struct leg{
+    uint phiServo; // forward - back
+    uint thetaServo; // up - down
+
+    // set forward-back angle in radians
+    void setPhi(float angle){
+        setAngleRad(phiServo, angle);
+    };
+
+    // set up-down angle in radians
+    void setTheta(float angle){
+        setAngleRad(thetaServo, angle);
+    };
+
+
+    float phaseOffset = 0;
+
+    // two values representing the same thing in different units
+    // height is the offset in mm of the corner of the robot that holds this leg
+    // baseAngle is the servo angle that thetaServo needs to hold to achieve height
+    float height = 0, baseAngle = M_PI/2;
+}leg;
+
+
+
+// X forward, Y right, Z up
+const vec3 cornerFL = { BOT_LENGTH / 2, -BOT_WIDTH / 2, 0};
+const vec3 cornerBL = {-BOT_LENGTH / 2, -BOT_WIDTH / 2, 0};
+const vec3 cornerFR = { BOT_LENGTH / 2,  BOT_WIDTH / 2, 0};
+const vec3 cornerBR = {-BOT_LENGTH / 2,  BOT_WIDTH / 2, 0};
+
+//TODO: update
+leg legFL = {6, 7};
+leg legBL = {8, 9};
+leg legFR = {10, 11};
+leg legBR = {12, 13};
+
+leg* legs[4] = {
+    &legFL,
+    &legBL,
+    &legFR,
+    &legBR
+};
+
+//TODO: update
+// helps mirror servos to keep the math identical
+vec2 legOrientations[] ={
+    vec2{-1, 1},
+    vec2{-1, 1},
+    vec2{1, -1},
+    vec2{1, -1}
+};
+
+
+
+float getHeightAngle(float targetHeight){
+    // Inverse Kinematics iterative approach
+    /*
+    float tempAngle = 0;
+    float temp = 0; //middle
+    float range = M_PI_2;
+
+    // like a binary search
+    while(fabs(targetHeight - temp) > 5){ // if current is within 5mm of target
+        
+        if(targetHeight > temp)                 // check if we're over or under
+            tempAngle += range * 0.5f;
+        else
+            tempAngle -= range * 0.5f;
+
+        temp = sinf(tempAngle) * ARM_LENGTH;  // generate condition //expensive operation
+            range /= 2;                         // shrink search area
+    }
+
+    return tempAngle;
+    */
+    // TODO: add safety clamp
+    float value = maxf(minf(targetHeight / ARM_LENGTH, 1), -1);;
+    return asinf(value);
+}
+
+
+
+double phaseFloat = 0;
+float trotOffset[] = {0.5, 0, 0, 0.5};
+float walkOffset[] = {0, 0.25, 0.5, 0.75};
+
+
+// movement has two steps: swing and return
+// during swing, the leg traces a semicircle and brings itself forward
+// during return, the leg moves back and pushes the robot forward
+// The time for the two steps must always add to one
+// I've chosen to return time as 1 - swing time instead of giving it a new variable
+const float trotSwingTime = 0.25;
+const float walkSwingTime = 0.2;
+
+
+// first value is forward-back, 2nd is up-down
+vec2 getLegPos(float phase){
+    phase = minf(phase, 1);
+    if(robotState != moveType::still){
+        float swingTime = robotState == moveType::trot ? trotSwingTime : walkSwingTime;
+
+        if(phase < swingTime){
+            float phaseProgress = phase / swingTime;
+            float heightAngle = getHeightAngle((sinf(M_PI * phaseProgress)) * SWING_HEIGHT);
+            float strideAngle = cosf(M_PI * phaseProgress) * SWING_ARC_RAD;
+            return {strideAngle, heightAngle};
+        }
+        else{
+            float phaseProgress = (phase - swingTime) / (1.0f - swingTime);
+            float strideAngle = (2 * phaseProgress - 1) * SWING_ARC_RAD;  // the swing goes between 1 and -1 radians, this brings it from -1 back to 1
+            return {strideAngle, 0};
+        }
+    }
+    else return {0, 0};
+}
+
+
+
+
+// extracts one result out of matrix multiplication
+// the corner height after Pitch x Roll
+// assumes Z always starts at 0 AND corners are const
+float getCornerHeight(vec3 corner, float pitch, float roll){
+    float result =  -corner.x * sin(pitch) + 
+                    (corner.y * sin(roll) * cos(pitch));
+
+    //TODO: parametrize this clamp
+    // makes sure the requested height isn't out of bounds and leaves headroom for walking
+    return maxf(minf(result, ARM_LENGTH - 15), -ARM_LENGTH + 5);
+}
+
+// mm, deg, deg
+void setPose(float height, float pitch, float roll){
+    legFL.height = legOrientations[0].x * (height + getCornerHeight(cornerFL, pitch, roll));
+    legBL.height = legOrientations[1].x * (height + getCornerHeight(cornerBL, pitch, roll));
+    legFR.height = legOrientations[2].x * (height + getCornerHeight(cornerFR, pitch, roll));
+    legBR.height = legOrientations[3].x * (height + getCornerHeight(cornerBR, pitch, roll));
+
+    // TODO: once i begin testing servos, make the proper baseAgles negative
+    legFL.baseAngle = M_PI_2 + getHeightAngle(legFL.height);
+    legFR.baseAngle = M_PI_2 + getHeightAngle(legFR.height);
+    legBL.baseAngle = M_PI_2 + getHeightAngle(legBL.height);
+    legBR.baseAngle = M_PI_2 + getHeightAngle(legBR.height);
+}
+
+
+// TODO: there are 6 transitions which need to be accounted for
+// from home to swing
+// from home to return
+// from swing to return
+// from swing to home
+// from return to swing
+// from return to home
+// find a way to account for this, hopefully without edge cases
+// This is the function that applies phase offsets to the legs, TODO
+void setMovementType(moveType type){
+    
+}
+
+
+
+// defined as how much of a step is finished per second
+double timestep = 0;
+// -100 -> +100
+void setSpeed(float speed){
+
+    //TODO: currently doesn't support going backwards, clamped to forward for testing
+    speed = minf(100, maxf(0, speed));
+
+    // a timestep of 1 means the robot is going through an entire walk cycle once per second
+    // TODO: test and find a decent upper limit
+    timestep = speed / 200;
+}
+//TODO: on 0 throttle for half a second, return to standstill
+
+
+// yaw rate is a multiplier applied to stride length
+// positive makes left stride shorter, turning left, negative shortens right stride
+//TODO: implement
+void setHeading(float yawRate);
+
+
+
+
 //TODO: repurpose into a generic "RECEIVE" function regardless of receive method
 //TODO: finish this
-// will integrate with the crsf parser
+// will integrate with the upcoming crsf parser
 void receiveSerial(){
 
     // the current setup allows receiving messages over serial with USB
+    // this will be discarded later
     while (tud_cdc_available())
     {
         // Create a place to hold the incoming message
@@ -308,19 +371,19 @@ int main()
     initServo(servoPins, SERVO_CNT);
 
     
-    //i2c_init(I2C_PORT, 1000000); //Use i2c port with baud rate of 1Mhz
-    
-    //gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    //gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    //gpio_pull_up(I2C_SDA);
-    //gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    initDisplay();
 
+    /*
+    // loop used to showcase the faces of the robot    
+    int emote =  0;
+    while(true){
+        setFace(emotion(emote), display);
+        emote++;
+        emote%=9;
+        sleep_ms(2000);
+    }
+    */
 
-    //pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3C, pico_ssd1306::Size::W128xH64);
-    //display.setOrientation(false);
-
-    //setFace(emotion::normal, &display);
 
     /*
     // Enable wifi station
@@ -352,19 +415,10 @@ int main()
     //while(true){sleep_ms(1000);}
     
 
+
+    // LED turning off means setup stopped
+    // TODO: add #ifndef for pico and pico W boards
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-
-
-    // loop used to showcase the faces of the robot
-    /*
-    int emote =  0;
-    while(true){
-        setFace(emotion(emote), &display);
-        emote++;
-        emote%=9;
-        sleep_ms(2000);
-    }
-    */
 
     
 
@@ -373,11 +427,19 @@ int main()
     while (true) {
         receiveSerial(); //to add: setSpeed, setFace, setPose,
 
-        // the given motion is structured such that positive turns up and negative turns down
-        // everything is centered on 90 degrees, center of servo motion
-        // this means the only change needed is to turn the offset negative
-        // however, base angle does need to be computed differently
+        phaseFloat += timestep * (dt / 1000000.0);
 
+
+        // !!!!!!!!!!!!!!!!!!!!! IMPORTANT NOTE !!!!!!!!!!!!!!!!!!!!! 
+        // servos should ALWAYS work in offsets!
+        // up-down axis offsets from baseAngle
+        // forward-back axis offsets from M_PI_2
+        // when assembled, legs should extend out from the center of the servo's range
+
+
+
+        /*
+        // stored for reference
         // this sets the servo angles as starting positions + a fraction of the offset needed to reach the target
         // for phi, M_PI_2 is the baseAngle
         for(int i = 0; i < 4; i++){
@@ -395,11 +457,9 @@ int main()
                             (phaseFloat * 
                                 (legs[i]->target.y - legs[i]->start.y)))));
         }
+        */
+        //TODO: add new gait system logic in main loop
 
-        
-        phaseFloat += timestep * (dt / 1000000.0);
-
-        
 
         // accounts for the program somehow getting stuck
         while(phaseFloat >= 1.0)
